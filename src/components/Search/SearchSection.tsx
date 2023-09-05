2d2eb331891f2ee9dece8ccdf146a452abbad114
jihye yu <dbwlgp1yng@gmail.com>
2023-09-05 21:25:40 +0900

    feat: 입력값이 있을때만 렌더링 처리

--- src/components/Search/SearchSection.tsx
+++ src/components/Search/SearchSection.tsx
@@ -17,14 +17,18 @@ export default function SearchSection() {
         console.error(error);
       }
     };
-    fetchData(); // input 값이 변경될 때마다 fetchData 함수 호출
-  }, [inputText]); // inputText 상태를 의존성으로 지정
 
-  console.log(data);
+    if (inputText.length > 0) {
+      fetchData(); // inputText가 비어있지 않을 때만 fetchData 호출
+    } 
+  }, [inputText]);
+
   return (
     <>
       <SearchBar inputText={inputText} setInputText={setInputText} data={data} />
-      <SearchBox inputText={inputText} data={data} />
+      {inputText.length > 0 && (  // 비어있지 않을 때만 렌더링
+        <SearchBox inputText={inputText} data={data} />
+      )}
     </>
   );
 };
