a128c2baf761cc60c65e0975962a48b9bde7136c
jihye yu <dbwlgp1yng@gmail.com>
2023-09-05 23:32:49 +0900

    refactor: 검색어 없을 시 로직 변경

--- src/components/Search/SearchBox.tsx
+++ src/components/Search/SearchBox.tsx
@@ -4,18 +4,16 @@ type SearchBoxProps = {
   data: SearchWordType[] | null;
 };
 export default function SearchBox({ data }: SearchBoxProps) {
-  
+  if (!data || data.length === 0) {
+    return <p>검색어 없음</p>;
+  }
   return (
     <div>
       <p>추천 검색어</p>
       <ul>
-        {data && data.length > 0 ? (
-          data.map((item, index) => (
-            <li key={index}>{item.sickNm}</li>
-          ))
-        ) : (
-          <li>검색어 없음</li>
-        )}
+        {data.map((item, index) => (
+          <li key={index}>{item.sickNm}</li>
+        ))}
       </ul>
     </div>
   );
