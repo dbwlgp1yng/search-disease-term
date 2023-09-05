7a75dca9d533437827e566293a8ba385cf22127d
jihye yu <dbwlgp1yng@gmail.com>
2023-09-05 21:39:55 +0900

    feat: GlobalStyle 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { router } from './router/Router';
 import { RouterProvider } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
