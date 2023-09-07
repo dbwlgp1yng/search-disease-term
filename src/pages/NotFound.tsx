5502e8ce583ca8bc45b690416575ece2b8cbdff3
jihye yu <dbwlgp1yng@gmail.com>
2023-09-07 15:59:45 +0900

    design: add notfound default style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Header from "../components/common/Header/Header"
 
 
@@ -5,10 +6,26 @@ export default function NotFound() {
   return (
     <>
       <Header />
-      <p>
-        404 Not Found<br />
-        페이지를 찾을 수 없습니다.
-      </p>
+      <StyledNotFound>
+        <h2>404 Not Found</h2>
+        <p>페이지를 찾을 수 없습니다.</p>
+      </StyledNotFound>
     </>
   );
 }
+
+const StyledNotFound = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 500px;
+  text-align: center;
+  background-color: #a0c8e9;
+  font-weight: bold;
+  
+  h2 {
+    font-size: 3rem;
+    margin-bottom: 2rem;
+  }
+`;
