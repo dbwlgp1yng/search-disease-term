a45733fb060843dd2400a1b3af1064b7817e9a36
jihye yu <dbwlgp1yng@gmail.com>
2023-09-07 22:26:16 +0900

    fix: router 경로 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { router } from './router/router';
+import { router } from '../src/router/router';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 
