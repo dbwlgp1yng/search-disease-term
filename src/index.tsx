be252c8cc96e26c5620e6c0d05249a6c228d6085
jihye yu <dbwlgp1yng@gmail.com>
2023-09-07 22:21:55 +0900

    fix: router 배포 에러 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { router } from './router/Router';
+import { router } from './router/router';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 
